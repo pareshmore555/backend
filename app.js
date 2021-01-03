594ff0bc8fb74e0950b28f0d9f0ebd53fbf2020b
paresh <pareshmore555@gmail.com>
2021-01-03 21:04:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+
+app.use(express.json())
+app.get("/",(req,res)=>{
+    res.send("welcome")
+})
+app.post("/user1",(req,res)=>{
+    
+    res.send(`creted your user ${req.body.name}`)
+})
+
+mongoose.connect("mongodb+srv://pareshmore:manchestor@123@cluster0.lhs8e.mongodb.net/<dbname>?retryWrites=true&w=majority",
+{useUnifieldTopology:true,useNewUrlParser:true},
+(req,res)=>{
+    console.log("connected to database")
+})
+app.listen(3001,()=>console.log("port is listening"))
